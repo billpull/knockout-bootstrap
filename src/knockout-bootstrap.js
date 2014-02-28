billpull/knockout-bootstrap

commit 4722c3a6c392e1d45a6871599037bae91c490aca

Add support for dynamic templates for popover

src/knockout-bootstrap.js
@@ -102,6 +102,9 @@ ko.bindingHandlers.popover = {
 		// set popover template id
 		var tmplId = popoverBindingValues.template;
 
+		// set data for template
+        var data = popoverBindingValues.data;
+
 		// set popover trigger
 		var trigger = 'click';
 
@@ -120,7 +123,19 @@ ko.bindingHandlers.popover = {
 		var placement = popoverBindingValues.placement;
 
 		// get template html
-		var tmplHtml = $('#' + tmplId).html();
+		if (!data) {
+          var tmplHtml = $('#' + tmplId).html();
+        } else {
+          var tmplHtml = function () {
+            var container = $('<div data-bind="template: { name: template, if: data, data: data }"></div>');
+
+            ko.applyBindings({
+                template: tmplId,
+                data: data
+            }, container[0]);
+            return container;
+          };
+        }
 
 		// create unique identifier to bind to
 		var uuid = guid();
@@ -172,8 +187,6 @@ ko.bindingHandlers.popover = {
 		
 			// if the popover is visible bind the view model to our dom ID
 			if($('#' + domId).is(':visible')){
-
-                ko.applyBindingsToDescendants(childBindingContext, $('#' + domId)[0]);
 
                 /* Since bootstrap calculates popover position before template is filled,
                  * a smaller popover height is used and it appears moved down relative to the trigger element.

commit b4353070d63fadf6dc43c22b1fa61c6909e92541

Fix bug in popover preventing bind to viewmodel properties.
This was caused by 4722c3a6c392e1d45a6871599037bae91c490aca

src/knockout-bootstrap.js
@@ -223,7 +223,9 @@ function setupKoBootstrap(koObject) {
                 $('.ko-popover').not(popoverInnerEl).parents('.popover').remove();
 
                 // if the popover is visible bind the view model to our dom ID
-                if ($('#' + domId).is(':visible')) {
+                if (popoverInnerEl.is(':visible')) {
+
+                    ko.applyBindingsToDescendants(childBindingContext, popoverInnerEl[0]);
 
                     /* Since bootstrap calculates popover position before template is filled,
                      * a smaller popover height is used and it appears moved down relative to the trigger element.
@@ -260,6 +262,9 @@ function setupKoBootstrap(koObject) {
                 $(document).on('click', '[data-dismiss="popover"]', function (e) {
                     popoverTriggerEl.popover('hide');
                 });
+
+                // Also tell KO *not* to bind the descendants itself, otherwise they will be bound twice
+                return { controlsDescendantBindings: true };
             });
         },
         options: {