027503f2b810a68759261321e029f34052bbea60
Mike Haas <mike@emken.co>
2014-03-02 23:17:13 -0600

    Set up a foundation for testing with karma, mocha and chai.

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var header = require('gulp-header');
+var karma = require('gulp-karma');
 var fs = require('fs');
 var pkg = require('./package.json');
 
 var paths = {
-    src: 'src/' + pkg.name + '.js'
+    src: 'src/' + pkg.name + '.js',
+    spec: 'spec/'
 };
 
 // Using a banner that is stored in a file to make it easier to work with.
@@ -18,7 +20,7 @@ try {
     throw new Error('There was an error reading in the banner.');
 }
 
-gulp.task('default', ['build']);
+gulp.task('default', ['watch']);
 
 gulp.task('lint', function() {
     gulp.src(paths.src)
@@ -27,10 +29,26 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('build', ['lint'], function() {
+gulp.task('build', ['lint', 'test'], function() {
     gulp.src(paths.src)
         .pipe(uglify())
         .pipe(concat(pkg.name + '.min.js'))
         .pipe(header(banner, { pkg: pkg, buildDate: gutil.date(new Date(), 'yyyy-mm-dd') }))
         .pipe(gulp.dest('build'));
+});
+
+gulp.task('test', function() {
+    gulp.src(paths.spec + '**/*-spec.js')
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'run'
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.src(paths.spec + '**/*-spec.js')
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'watch'
+        }));
 });
